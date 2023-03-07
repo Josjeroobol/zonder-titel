063e2af8b791dba776ab9aa181abb914f2e6587f
Josjeroobol <124696948+Josjeroobol@users.noreply.github.com>
2023-03-07 15:57:50 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,48 @@
+let kompasrichting = 0
+basic.forever(function () {
+    kompasrichting = input.compassHeading()
+})
+basic.forever(function () {
+    if (input.compassHeading() < 35 && input.compassHeading() > 135) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.compassHeading() < 135 && input.compassHeading() > 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.compassHeading() < 225 && input.compassHeading() > 315) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.compassHeading() < 45 && input.compassHeading() > 315) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
